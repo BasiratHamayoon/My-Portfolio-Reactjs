ccf8f7885247542848701d27a773262727639f3a
Basirat Hamayoon <basirathumayun@gmail.com>
2025-01-24 18:42:33 +0500

    Create some custome components for home, services, projects, about me and contact

---
+++ src/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+    const navItems = [
+        {
+            id: 1,
+            title: "Home",
+            url: "",
+        },
+        {
+            id: 2,
+            title: "Services",
+            url: "",
+        },
+
+    ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
